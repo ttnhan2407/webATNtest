0a1b42574d9b2d2fc818c0b8d352004968384b91
ttnhan2407 <heinekencn@gmail.com>
2021-11-09 15:56:25 +0700

    Summary
    11/9

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 
-    
-
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="Vinyl Template">
@@ -365,9 +363,6 @@ include_once("connection.php");
 
 ?>
    
-
-    
-
     <!-- Footer Section Begin -->
     <footer class="footer spad">
         <div class="container">
@@ -390,24 +385,12 @@ include_once("connection.php");
                         <ul>
                             <li><a href="#">About Us</a></li>
                             <li><a href="#">About Our Shop</a></li>
-                            <li><a href="#">Secure Shopping</a></li>
-                            <li><a href="#">Delivery infomation</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Our Sitemap</a></li>
-                        </ul>
-                        <ul>
-                            <li><a href="#">Who We Are</a></li>
-                            <li><a href="#">Our Services</a></li>
-                            <li><a href="#">Projects</a></li>
-                            <li><a href="#">Contact</a></li>
-                            <li><a href="#">Innovation</a></li>
-                            <li><a href="#">Testimonials</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-12">
                     <div class="footer__widget">
-                        <h6>Join Our Newsletter Now</h6>
+                        <h6>Thank you for your interest</h6>
                         <p>Get E-mail updates about our latest shop and special offers.</p>
                         <form action="#">
                             <input type="text" placeholder="Enter your mail">
@@ -483,8 +466,3 @@ include_once("connection.php");
     
     
     ?>
-    
-    
-
-
-
--- shop-grid.php
+++ shop-grid.php
@@ -9,10 +9,7 @@
                             <i class="fa fa-bars"></i>
                             <span>All departments</span>
                         </div>
-                        <ul>
-                        <?php Department($conn ); ?>
-                            
-                        </ul>
+
                     </div>
                 </div>
                 <div class="col-lg-9">
@@ -62,166 +59,6 @@
     <section class="product spad">
         <div class="container">
             <div class="row">
-                <div class="col-lg-3 col-md-5">
-                    <div class="sidebar">
-                        <div class="sidebar__item">
-                            <h4>Department</h4>
-                            <ul>
-                            
-                            <?php Department($conn ); ?>
-                                
-                            </ul>
-                        </div>
-                        <div class="sidebar__item">
-                            <h4>Price</h4>
-                            <div class="price-range-wrap">
-                                <div class="price-range ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content"
-                                    data-min="10" data-max="540">
-                                    <div class="ui-slider-range ui-corner-all ui-widget-header"></div>
-                                    <span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default"></span>
-                                    <span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default"></span>
-                                </div>
-                                <div class="range-slider">
-                                    <div class="price-input">
-                                        <input type="text" id="minamount">
-                                        <input type="text" id="maxamount">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="sidebar__item sidebar__item__color--option">
-                            <h4>Colors</h4>
-                            <div class="sidebar__item__color sidebar__item__color--white">
-                                <label for="white">
-                                    White
-                                    <input type="radio" id="white">
-                                </label>
-                            </div>
-                            <div class="sidebar__item__color sidebar__item__color--gray">
-                                <label for="gray">
-                                    Gray
-                                    <input type="radio" id="gray">
-                                </label>
-                            </div>
-                            <div class="sidebar__item__color sidebar__item__color--red">
-                                <label for="red">
-                                    Red
-                                    <input type="radio" id="red">
-                                </label>
-                            </div>
-                            <div class="sidebar__item__color sidebar__item__color--black">
-                                <label for="black">
-                                    Black
-                                    <input type="radio" id="black">
-                                </label>
-                            </div>
-                            <div class="sidebar__item__color sidebar__item__color--blue">
-                                <label for="blue">
-                                    Blue
-                                    <input type="radio" id="blue">
-                                </label>
-                            </div>
-                            <div class="sidebar__item__color sidebar__item__color--green">
-                                <label for="green">
-                                    Green
-                                    <input type="radio" id="green">
-                                </label>
-                            </div>
-                        </div>
-                        <div class="sidebar__item">
-                            <h4>Popular</h4>
-                            <div class="sidebar__item__size">
-                                <label for="33RPM">
-                                    33RPM
-                                    <input type="radio" id="33RPM">
-                                </label>
-                            </div>
-                            <div class="sidebar__item__size">
-                                <label for="45RPM">
-                                    45RPM
-                                    <input type="radio" id="45RPM">
-                                </label>
-                            </div>
-                            <div class="sidebar__item__size">
-                                <label for="Signature">
-                                    Signature
-                                    <input type="radio" id="Signature">
-                                </label>
-                            </div>
-                            <div class="sidebar__item__size">
-                                <label for="Trend">
-                                    Trend
-                                    <input type="radio" id="Trend">
-                                </label>
-                            </div>
-                        </div>
-                        <div class="sidebar__item">
-                            <div class="latest-product__text">
-                                <h4>Latest Products</h4>
-                                <div class="latest-product__slider owl-carousel">
-                                    <div class="latest-prdouct__slider__item">
-                                        <a href="#" class="latest-product__item">
-                                            <div class="latest-product__item__pic">
-                                                <img src="ATNimg/ace.jfif" alt="">
-                                            </div>
-                                            <div class="latest-product__item__text">
-                                                <h6></h6>
-                                                <span></span>
-                                            </div>
-                                        </a>
-                                        <a href="#" class="latest-product__item">
-                                            <div class="latest-product__item__pic">
-                                                <img src="ATNimg/luffy.jfif" alt="">
-                                            </div>
-                                            <div class="latest-product__item__text">
-                                                <h6></h6>
-                                                <span></span>
-                                            </div>
-                                        </a>
-                                        <a href="#" class="latest-product__item">
-                                            <div class="latest-product__item__pic">
-                                                <img src="ATNimg/Levi.jfif" alt="">
-                                            </div>
-                                            <div class="latest-product__item__text">
-                                                <h6></h6>
-                                                <span></span>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <div class="latest-prdouct__slider__item">
-                                        <a href="#" class="latest-product__item">
-                                            <div class="latest-product__item__pic">
-                                                <img src="ATNimg/Mikey.jpg" alt="">
-                                            </div>
-                                            <div class="latest-product__item__text">
-                                                <h6></h6>
-                                                <span></span>
-                                            </div>
-                                        </a>
-                                        <a href="#" class="latest-product__item">
-                                            <div class="latest-product__item__pic">
-                                                <img src="ATNimg/pika.jfif" alt="">
-                                            </div>
-                                            <div class="latest-product__item__text">
-                                                <h6></h6>
-                                                <span></span>
-                                            </div>
-                                        </a>
-                                        <a href="#" class="latest-product__item">
-                                            <div class="latest-product__item__pic">
-                                                <img src="ATNimg/zr.jfif" alt="">
-                                            </div>
-                                            <div class="latest-product__item__text">
-                                                <h6></h6>
-                                                <span></span>
-                                            </div>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <div class="col-lg-9 col-md-7">
                     <div class="product__discount">
                         <div class="section-title product__discount__title">
@@ -338,9 +175,6 @@
                         
                     </div>
                     <div class="product__pagination">
-                        <a href="#">1</a>
-                        <a href="#">2</a>
-                        <a href="#">3</a>
                         
                     </div>
                 </div>
