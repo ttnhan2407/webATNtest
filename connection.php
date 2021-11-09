2f32eb879cc534889178c11ec932170369fbca0b
ttnhan2407 <heinekencn@gmail.com>
2021-11-09 10:33:50 +0700

    Update connection
    11/9

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = pg_connect("postgres://eqsswfxaphixgx:79c5717c086ecfdb796105255f2a0f48e0ec8b5c2278c2e680b24bb8689abc65@ec2-54-145-139-208.compute-1.amazonaws.com:5432/d200jeu77acrei");
+$conn = pg_connect("postgres://zmwunuawhfncoa:3cc4307784e2497e90c925bf9b032ab947aa347939d70d422f621668d9ae2827@ec2-35-169-49-157.compute-1.amazonaws.com:5432/d2jvl120enokg6");
 	echo 'Connected Successfully!!!';
 	if(!$conn)
 	{
