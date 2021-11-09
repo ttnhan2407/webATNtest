f9034018bbd6133bc853e02447eee1524cea8288
ttnhan2407 <heinekencn@gmail.com>
2021-11-09 17:37:29 +0700

    Update shop-grid
    11/9

--- shop-grid.php
+++ shop-grid.php
@@ -21,6 +21,169 @@
     <section class="product spad">
         <div class="container">
             <div class="row">
+            <section class="product spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-5">
+                    <div class="sidebar">
+                        <div class="sidebar__item">
+                            <h4>Department</h4>
+                            <ul>
+                            
+                            <?php Department($conn ); ?>
+                                
+                            </ul>
+                        </div>
+                        <div class="sidebar__item">
+                            <h4>Price</h4>
+                            <div class="price-range-wrap">
+                                <div class="price-range ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content"
+                                    data-min="10" data-max="540">
+                                    <div class="ui-slider-range ui-corner-all ui-widget-header"></div>
+                                    <span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default"></span>
+                                    <span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default"></span>
+                                </div>
+                                <div class="range-slider">
+                                    <div class="price-input">
+                                        <input type="text" id="minamount">
+                                        <input type="text" id="maxamount">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="sidebar__item sidebar__item__color--option">
+                            <h4>Colors</h4>
+                            <div class="sidebar__item__color sidebar__item__color--white">
+                                <label for="white">
+                                    White
+                                    <input type="radio" id="white">
+                                </label>
+                            </div>
+                            <div class="sidebar__item__color sidebar__item__color--gray">
+                                <label for="gray">
+                                    Gray
+                                    <input type="radio" id="gray">
+                                </label>
+                            </div>
+                            <div class="sidebar__item__color sidebar__item__color--red">
+                                <label for="red">
+                                    Red
+                                    <input type="radio" id="red">
+                                </label>
+                            </div>
+                            <div class="sidebar__item__color sidebar__item__color--black">
+                                <label for="black">
+                                    Black
+                                    <input type="radio" id="black">
+                                </label>
+                            </div>
+                            <div class="sidebar__item__color sidebar__item__color--blue">
+                                <label for="blue">
+                                    Blue
+                                    <input type="radio" id="blue">
+                                </label>
+                            </div>
+                            <div class="sidebar__item__color sidebar__item__color--green">
+                                <label for="green">
+                                    Green
+                                    <input type="radio" id="green">
+                                </label>
+                            </div>
+                        </div>
+                        <div class="sidebar__item">
+                            <h4>Popular</h4>
+                            <div class="sidebar__item__size">
+                                <label for="33RPM">
+                                    33RPM
+                                    <input type="radio" id="33RPM">
+                                </label>
+                            </div>
+                            <div class="sidebar__item__size">
+                                <label for="45RPM">
+                                    45RPM
+                                    <input type="radio" id="45RPM">
+                                </label>
+                            </div>
+                            <div class="sidebar__item__size">
+                                <label for="Signature">
+                                    Signature
+                                    <input type="radio" id="Signature">
+                                </label>
+                            </div>
+                            <div class="sidebar__item__size">
+                                <label for="Trend">
+                                    Trend
+                                    <input type="radio" id="Trend">
+                                </label>
+                            </div>
+                        </div>
+                        <div class="sidebar__item">
+                            <div class="latest-product__text">
+                                <h4>Latest Products</h4>
+                                <div class="latest-product__slider owl-carousel">
+                                    <div class="latest-prdouct__slider__item">
+                                        <a href="#" class="latest-product__item">
+                                            <div class="latest-product__item__pic">
+                                                <img src="ATNtoy/ace.jpg" alt="">
+                                            </div>
+                                            <div class="latest-product__item__text">
+                                                <h6></h6>
+                                                <span></span>
+                                            </div>
+                                        </a>
+                                        <a href="#" class="latest-product__item">
+                                            <div class="latest-product__item__pic">
+                                                <img src="ATNtoy/luffy.jpg" alt="">
+                                            </div>
+                                            <div class="latest-product__item__text">
+                                                <h6></h6>
+                                                <span></span>
+                                            </div>
+                                        </a>
+                                        <a href="#" class="latest-product__item">
+                                            <div class="latest-product__item__pic">
+                                                <img src="ATNtoy/Levi.jpg" alt="">
+                                            </div>
+                                            <div class="latest-product__item__text">
+                                                <h6></h6>
+                                                <span></span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="latest-prdouct__slider__item">
+                                        <a href="#" class="latest-product__item">
+                                            <div class="latest-product__item__pic">
+                                                <img src="ATNtoy/Mikey.jpg" alt="">
+                                            </div>
+                                            <div class="latest-product__item__text">
+                                                <h6></h6>
+                                                <span></span>
+                                            </div>
+                                        </a>
+                                        <a href="#" class="latest-product__item">
+                                            <div class="latest-product__item__pic">
+                                                <img src="ATNtoy/pika.jpg" alt="">
+                                            </div>
+                                            <div class="latest-product__item__text">
+                                                <h6></h6>
+                                                <span></span>
+                                            </div>
+                                        </a>
+                                        <a href="#" class="latest-product__item">
+                                            <div class="latest-product__item__pic">
+                                                <img src="ATNtoy/zr.jpg" alt="">
+                                            </div>
+                                            <div class="latest-product__item__text">
+                                                <h6></h6>
+                                                <span></span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="col-lg-9 col-md-7">
                     <div class="product__discount">
                         <div class="section-title product__discount__title">
