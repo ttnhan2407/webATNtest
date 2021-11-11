4be9fd60690253a0974f114a688995ba4df68c70
ttnhan2407 <heinekencn@gmail.com>
2021-11-11 23:32:19 +0700

    Summary
    11/11

--- shop-grid.php
+++ shop-grid.php
@@ -34,14 +34,6 @@
                                 
                             </ul>
                         </div>
-                        <div class="sidebar__item">
-                            <h4>Branch</h4>
-                            <ul>
-                            
-                            <?php departmentbranch($conn ); ?>
-                                
-                            </ul>
-                        </div>
                     </div>
                 </div>
                 <div class="col-lg-9 col-md-7">
