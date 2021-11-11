7e662175e2d7fa8777fbe7777ab74cc19a7d3464
ttnhan2407 <heinekencn@gmail.com>
2021-11-11 17:32:08 +0700

    Update branch
    11/11

--- update_branch.php
+++ update_branch.php
@@ -1,52 +1,4 @@
- <!-- Hero Section Begin -->
- <section class="hero hero-normal">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="hero__branch">
-                        <div class="hero__branch__all">
-                            <i class="fa fa-bars"></i>
-                            <span>All departments</span>
-                            
-                            
-                        </div>
-                        
-                        <ul>
-                        <li ><a  href="?page=pm">All</a></li>
 
-                        <?php Branch_List($conn ); ?>
-                            
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-9">
-                    <div class="hero__search">
-                        <div class="hero__search__form">
-                            <form action="#">
-                                <div class="hero__search__branch">
-                                    All branch
-                                    <span class="arrow_carrot-down"></span>
-                                    
-                                </div>
-                                <input type="text" placeholder="What do yo u need?">
-                                <button type="submit" class="site-btn">SEARCH</button>
-                            </form>
-                        </div>
-                        <div class="hero__search__phone">
-                            <div class="hero__search__phone__icon">
-                                <i class="fa fa-phone"></i>
-                            </div>
-                            <div class="hero__search__phone__text">
-                                <h5>081 537 8057</h5>
-                                <span>Support 24/7 time</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Hero Section End -->
 
     <!-- Breadcrumb Section Begin -->
     <section class="breadcrumb-section set-bg" data-setbg="ATNimg/one.jpg">
@@ -74,8 +26,8 @@
 			$id = $_GET['id'];
 			$result = pg_query($conn, "SELECT * from branch where branch_id = '$id'");
 			$row = pg_fetch_array($result, NULL, PGSQL_ASSOC);
-			$cat_id = $row['branch_id'];
-			$cat_name = $row['branch_name'];
+			$branch_id = $row['branch_id'];
+			$branch_name = $row['branch_name'];
 	?>
 <div class="container">
 	<h2>Updating Product Branch</h2>
@@ -122,7 +74,7 @@
 			}
 			else
 			{
-				pg_query($conn, "UPDATE Branch set branch_name = '$name' where branch_id = '$id'");
+				pg_query($conn, "UPDATE branch set branch_name = '$name' where branch_id = '$id'");
 				echo '<meta http-equiv="refresh" content="0;URL =?page=branch"/>';
 			}
 		}
